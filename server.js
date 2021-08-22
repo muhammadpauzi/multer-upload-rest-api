479bb770cbf3dc43d8dca274fca77d4edd3070e2
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-08-22 22:14:49 +0700

    Add api's endpoint to get image's path

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { readdir } = require('fs');
+const { join } = require('path');
 const { response, uploadFile } = require('./utils');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const BASEURL = 'http://localhost:5000';
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'OK'
+    });
+});
+
+app.get('/images', (req, res) => {
+    const directoryPath = join(__dirname, 'uploads');
+    readdir(directoryPath, (err, files) => {
+        if (err) {
+            return response(res, 500, {
+                message: "Something went wrong",
+            });
+        }
+
+        let fileInfos = [];
+        files.map(file => {
+            fileInfos = [...fileInfos, { name: file, url: join(BASEURL, 'uploads', file) }];
+        });
+
+        return response(res, 200, {
+            files: fileInfos
+        });
     });
 });
 
